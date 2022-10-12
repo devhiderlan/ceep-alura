5869da8330889c76935ba5ac4abb125300b953ef
devhiderlan <dutrahiderlan@gmail.com>
2022-10-11 21:13:13 -0300

    Refatoramento do código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,4 @@
-import ButtonConclude from './components/concludeTask.js'
-import ButtonDelete from './components/deleteTask.js'
-
-const createTask = event => {
-  event.preventDefault()
-
-  const list = document.querySelector('[data-list]')
-  const input = document.querySelector('[data-form-input]')
-  const valueInput = input.value
-
-  const task = document.createElement('li')
-  task.classList.add('task')
-
-  const content = `<p class="content">${valueInput}</p>`
-  task.innerHTML = content
-  task.appendChild(ButtonConclude())
-  task.appendChild(ButtonDelete())
-  list.appendChild(task)
-  input.value = ''
-}
+import {handleNewItem} from './components/createTask.js'
 
 const newTask = document.querySelector('[data-form-button]')
-newTask.addEventListener('click', createTask)
+newTask.addEventListener('click', handleNewItem)
