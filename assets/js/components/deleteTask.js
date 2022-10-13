e1456d747736e67c5d38a810a5484bcba249603f
devhiderlan <dutrahiderlan@gmail.com>
2022-10-13 16:13:54 -0300

    Criada função para deletar tarefa no localStorage

--- assets/js/components/deleteTask.js
+++ assets/js/components/deleteTask.js
@@ -1,15 +1,19 @@
-const ButtonDelete = () => {
+const deleteTask = (update, id) => {
+  const index = id
+  const insertedTask = JSON.parse(localStorage.getItem('tasks'))
+
+  insertedTask.splice(index, 1)
+  localStorage.setItem('tasks', JSON.stringify(insertedTask))
+
+  update()
+}
+
+const ButtonDelete = (update, id) => {
   const buttonDelete = document.createElement('button')
   buttonDelete.classList.add('delete-button')
   buttonDelete.innerText = 'Deletar'
-  buttonDelete.addEventListener('click', deleteTask)
+  buttonDelete.addEventListener('click', () => deleteTask(update, id))
   return buttonDelete
-}
-
-const deleteTask = event => {
-  const buttonDelete = event.target
-  const taskDeleted = buttonDelete.parentElement
-  taskDeleted.remove()
 }
 
 export default ButtonDelete
